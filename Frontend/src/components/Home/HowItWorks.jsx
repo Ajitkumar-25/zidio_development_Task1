97c9afd5a8eb8e922a3770336e2c8687e7ef6360
Mohit Garg <mgarg1557@gmail.com>
2024-06-13 14:57:01 +0530

    Created Home and Job Pages

---
+++ Frontend/src/components/Home/HowItWorks.jsx
@@ -0,0 +1,40 @@
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>How JobZee Works</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus />
+              <p>Create Account</p>
+              <p>
+              Create an account to connect with top employers and discover job opportunities tailored to you.
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage />
+              <p>Find a Job/Post a Job</p>
+              <p>
+              Join us to discover tailored job opportunities or find the perfect candidate for your company.
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend />
+              <p>Apply For Job/Recruit Suitable Candidates</p>
+              <p>
+              Connect with top employers or find the perfect candidate for your needs. Join us today!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
